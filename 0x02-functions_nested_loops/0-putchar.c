891024dd6ef4beb60c809216a4c4ddf6cb4a9e7d
Tope Awe <blessedtopsy7@gmail.com>
2022-07-05 08:08:58 -0700

    still on loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,23 @@
-#Include "main.h"
+#include "main.h"
+
 
 /**
- * main - prints putchar
- * Return: always 0
- *\
- Int main (void)
- {
-      _putchar ('_');
-      _putchar ('p');
-      _putchar ('u');
-      _putchar ('t');
-      _putchar ('c');
-      _putchar ('h');
-      _putchar ('a');
-      _putchar ('r');
-      _putchar ('\n');
-      return (0);
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
