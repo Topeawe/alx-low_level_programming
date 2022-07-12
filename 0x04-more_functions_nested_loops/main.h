8e5ea64e4a0dda93431605d6c50817f5414a2278
Tope Awe <blessedtopsy7@gmail.com>
2022-07-11 20:25:38 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_triangle(int size);
 void print_number(int n);
+
+
 #endif
